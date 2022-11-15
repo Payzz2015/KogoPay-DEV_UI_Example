88dafb4637a91800b27b7ae3bab6768731e9d1ed
Teerapat Lekaumphan <51058890+payzz123z@users.noreply.github.com>
2022-11-16 02:38:30 +0700

    Initial commit

---
+++ app/src/main/java/com/teerapat/kogopay_devuiexample/LoadingScreenActivity.kt
@@ -0,0 +1,29 @@
+package com.teerapat.kogopay_devuiexample
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+
+class LoadingScreenActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading_screen)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },10000)
+    }
+}
